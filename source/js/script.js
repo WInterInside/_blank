96aee2f7f8222bdbba5497556a64369b490ffb38
WinterInside <de_animator@mail.ru>
2021-10-16 21:24:05 +0300

    first day at work

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,30 @@
-var menu = document.querySelector('.nav__menu');
-var nav = document.querySelector('.nav__list');
+var header = document.getElementById('nav');
+var links = header.getElementsByClassName('navigation__link');
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function() {
+    var current = document.getElementsByClassName('navigation__link--current');
+    current[0].className = current[0].className.replace(' navigation__link--current', '');
+    this.className += ' navigation__link--current';
+  });
+}
 
-nav.classList.add('nav__list--closed');
+var menu = document.querySelector('.navigation__menu');
+var nav = document.querySelector('.navigation__list');
+// var menuLine = document.querySelector('.navigation__line');
+
+nav.classList.add('navigation__list--closed');
+menu.classList.add('navigation__menu--off');
 
 menu.addEventListener('click', function() {
-    if (nav.classList.contains('nav__list--closed')) {
-    nav.classList.remove('nav__list--closed');
-    nav.classList.add('nav__list--opened');
-  } else {
-    nav.classList.remove('nav__list--opened');
-    nav.classList.add('nav__list--closed');
-  }
+  if (nav.classList.contains('navigation__list--closed')) {
+  nav.classList.remove('navigation__list--closed');
+  nav.classList.add('navigation__list--opened');
+  menu.classList.remove('navigation__menu--off');
+  menu.classList.add('navigation__menu--on');
+} else {
+  nav.classList.remove('navigation__list--opened');
+  nav.classList.add('navigation__list--closed');
+  menu.classList.remove('navigation__menu--on');
+  menu.classList.add('navigation__menu--off');
+}
 });
