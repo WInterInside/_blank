028cc94cde6605fee25c8d4609d7023863d6fc5b
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-03-29 19:58:57 +0300

    fix previev names

--- source/js/script.js
+++ source/js/script.js
@@ -8,20 +8,20 @@ for (var i = 0; i < links.length; i++) {
   });
 }
 
-const anchors = document.querySelectorAll('a[href*="#"]')
+// const anchors = document.querySelectorAll('a[href*="#"]')
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault()
 
-    const blockID = anchor.getAttribute('href').substr(1)
+//     const blockID = anchor.getAttribute('href').substr(1)
 
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
+//     document.getElementById(blockID).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     })
+//   })
+// }
 
 let scrollpos = window.scrollY
 const shadow = document.querySelector("header")
