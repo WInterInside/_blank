3b240c310380d59770f4e5b07b2b5053faa5c4a4
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-03-29 14:15:07 +0300

    add new positions to portfolio. some fixes

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,21 @@ for (var i = 0; i < links.length; i++) {
   });
 }
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
 let scrollpos = window.scrollY
 const shadow = document.querySelector("header")
 const header_height = header.offsetHeight
@@ -21,70 +36,6 @@ window.addEventListener('scroll', function() {
   if (scrollpos >= header_height) { add_class_on_scroll() }
   else { remove_class_on_scroll() }
 })
-
-function Tabs() {
-  var bindAll = function() {
-    var menuElements = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < menuElements.length ; i++) {
-      menuElements[i].addEventListener('click', change, false);
-    }
-  }
-
-  var clear = function() {
-    var menuElements = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < menuElements.length ; i++) {
-      menuElements[i].classList.remove('active');
-      var id = menuElements[i].getAttribute('data-tab');
-      document.getElementById(id).classList.remove('active');
-    }
-  }
-
-  var change = function(e) {
-    clear();
-    e.target.classList.add('active');
-    e.preventDefault();
-    var id = e.currentTarget.getAttribute('data-tab');
-    document.getElementById(id).classList.add('active');
-  }
-
-  bindAll();
-}
-
-window.addEventListener("DOMContentLoaded", function() {
-  [].forEach.call( document.querySelectorAll('.form__input--tel'), function(input) {
-  var keyCode;
-  function mask(event) {
-      event.keyCode && (keyCode = event.keyCode);
-      var pos = this.selectionStart;
-      if (pos < 3) event.preventDefault();
-      var matrix = "+7 (___) ___-__-__",
-          i = 0,
-          def = matrix.replace(/\D/g, ""),
-          val = this.value.replace(/\D/g, ""),
-          new_value = matrix.replace(/[_\d]/g, function(a) {
-              return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-          });
-      i = new_value.indexOf("_");
-      if (i != -1) {
-          i < 5 && (i = 3);
-          new_value = new_value.slice(0, i)
-      }
-      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-          function(a) {
-              return "\\d{1," + a.length + "}"
-          }).replace(/[+()]/g, "\\$&");
-      reg = new RegExp("^" + reg + "$");
-      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-      if (event.type == "blur" && this.value.length < 5)  this.value = ""
-  }
-
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-  input.addEventListener("blur", mask, false);
-  input.addEventListener("keydown", mask, false)
-
-});
-});
 
 var menu = document.querySelector(".navigation__menu");
 var nav = document.querySelector(".navigation__list");
