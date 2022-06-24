45ca0e348bdaa46f7e2cf13d003f766ab1110bbc
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-06-24 19:12:43 +0300

    add new sections etc

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,69 @@
+"use strict"
+
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+
+  // инициализация
+  function digitsCountersInit(digitsCountersItems) {
+    let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+
+    console.log (digitsCounters);
+
+    if (digitsCounters) {
+      digitsCounters.forEach(digitsCounter => {
+        digitsCountersItemsAnimate(digitsCounter);
+      });
+    }
+  }
+
+// анимация
+  function digitsCountersItemsAnimate(digitsCounter) {
+    let startTimestamp = null;
+    const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 1500;
+    const startValue = parseInt(digitsCounter.innerHTML);
+    const startPosition = 0;
+    const step = (timestamp) =>  {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  // пуск при загрузке страници
+  // digitsCountersInit();
+
+
+  // пуск при появлении в поле видимости
+  let options = {
+    threshold: 0.3
+  }
+  let observer = new IntersectionObserver ((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const targetElement = entry.target;
+        const digitsCountersItems = targetElement.querySelectorAll("[data-digits-counter]");
+
+        if (digitsCountersItems.length) {
+          digitsCountersInit(digitsCountersItems);
+        }
+          // отключить отслеживание после сработки
+          observer.unobserve(targetElement);
+      }
+    });
+  }, options);
+
+  let sections = document.querySelectorAll(".page__row");
+  if (sections.length) {
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+  }
+}
+
 var header = document.getElementById("nav");
 var links = header.getElementsByClassName("navigation__link");
 for (var i = 0; i < links.length; i++) {
@@ -7,21 +73,6 @@ for (var i = 0; i < links.length; i++) {
     this.className += " navigation__link--current";
   });
 }
-
-// const anchors = document.querySelectorAll('a[href*="#"]')
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
-
-//     const blockID = anchor.getAttribute('href').substr(1)
-
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
 
 let scrollpos = window.scrollY
 const shadow = document.querySelector("header")
@@ -55,4 +106,15 @@ menu.addEventListener("click", function() {
     menu.classList.remove("navigation__menu--on");
     menu.classList.add("navigation__menu--off");
   }
+});
+
+$(document).ready(function(){
+  $('a[href^="#"]').bind("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $(anchor.attr('href')).offset().top - 50
+      }, 1000);
+      e.preventDefault();
+  });
+  return false;
 });
